eb814caebbfed78d39fb7bb8007b4075f8509341
Andy <Andy@DESKTOP-7IR42LL>
2020-02-23 20:35:47 +0800

    homework1

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Calculator
+    {
+        public static double DoOperation(double num1, double num2, string op)
+        {
+            double res = double.NaN;
+            switch (op)
+            {
+                case "+":
+                    res = num1 + num2;
+                    break;
+                case "-":
+                    res = num1 - num2;
+                    break;
+                case "*":
+                    res = num1 * num2;
+                    break;
+                case "/":
+                    if (num2 != 0)
+                    {
+                        res = num1 / num2;
+                    }
+                    break;
+                default:
+                    break;
+            }
+            return res;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool end = false;
+            while (!end)
+            {
+                string Input1 = "";
+                string Input2 = "";
+                double res = 0;
+
+                Console.WriteLine("请输入第一个数字并按下回车键");
+                Input1 = Console.ReadLine();
+                double Clean1 = 0;
+                while (!double.TryParse(Input1, out Clean1))
+                {
+                    Console.Write("请输入有效的数字:");
+                    Input1 = Console.ReadLine();
+                }
+
+                Console.WriteLine("请输入第二个数字并按下回车键");
+                Input2 = Console.ReadLine();
+                double Clean2 = 0;
+                while (!double.TryParse(Input2, out Clean2))
+                {
+                    Console.Write("请输入有效的数字:");
+                    Input2 = Console.ReadLine();
+                }
+
+                Console.WriteLine("请选择运算符并按回车键");
+
+                string op = Console.ReadLine();
+                res = Calculator.DoOperation(Clean1, Clean2, op);
+
+                if (!double.IsNaN(res))
+                    Console.WriteLine("计算结果: {0:0.###}\n", res);
+                else
+                {
+                    Console.WriteLine("此计算过程出现错误\n");
+                }
+
+                Console.Write("按n退出程序,或按任意键以继续:");
+                if (Console.ReadLine() == "n") 
+                    end = true;
+                Console.WriteLine("\n"); 
+            }
+            return;
+        }
+    }
+}
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }       
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string Input1 = "";
+            string Input2 = "";
+
+            Input1 = textBox1.Text;
+            double num1 = 0;
+            //if (Input1==string.NaN)
+            //{
+            //    MessageBox.Show("请在第一个框中输入数字");
+            //    Input1 = textBox1.Text;
+            //}
+            if (!double.TryParse(Input1, out num1))
+            {
+                MessageBox.Show("请输入有效的数字");
+                Input1 = textBox1.Text;
+            }
+            else
+            {
+                Input1 = textBox1.Text;
+            }            
+
+            Input2 = textBox2.Text;
+            double num2 = 0;
+            if (!double.TryParse(Input2, out num2))
+            {
+                MessageBox.Show("请输入有效的数字");
+                Input2 = textBox2.Text;
+            }
+            else
+            {
+                Input2 = textBox2.Text;
+            }
+          
+            if (radioButton1.Checked)
+            {               
+                textBox3.Text = (num1 + num2).ToString();
+            }
+            else if (radioButton2.Checked)
+            {
+                textBox3.Text = (num1 - num2).ToString();
+            }
+            else if (radioButton3.Checked)
+            {
+                textBox3.Text = (num1 * num2).ToString();
+            }
+            else if (radioButton4.Checked)
+            {
+                if (num2 == 0)
+                {
+                    MessageBox.Show("请输入非零数");
+                }
+                textBox3.Text = (num1 / num2).ToString();
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
