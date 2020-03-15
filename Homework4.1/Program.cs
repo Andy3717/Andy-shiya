da39a8fd982643eaaa49c7a37a3c77c55d8bdd64
Andy <Andy@DESKTOP-7IR42LL>
2020-03-15 22:54:32 +0800

    Homework4

---
+++ Homework4.1/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericApplication
+{
+    public class Node<T>
+    {
+        public Node<T> Next { get; set; }
+        public T Data { get; set; }
+        public Node(T t)
+        {
+            Next = null;
+            Data = t;
+        }
+    }
+    public class GenericList<T>
+    {
+        private Node<T> head;
+        private Node<T> tail;
+        public GenericList()
+        {
+            head = null;
+            tail = null;
+        }
+        public Node<T> Head { get => head; }
+        public void Add(T t)
+        {
+            Node<T> n = new Node<T>(t);
+            if (tail == null)
+            {
+                head = tail = n;
+            }
+            else
+            {
+                tail.Next = n;
+                tail = n;
+            }
+        }
+        public void ForEach(Action<T> action)
+        {
+            Node<T> n = head;
+            while (n != null)
+            {
+                action(n.Data);
+                n = n.Next;
+            }
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0, max = int.MinValue, min = int.MaxValue;
+            GenericList<int> intlist = new GenericList<int>();
+            for (int i = 0; i < 10; i++)
+            {
+                intlist.Add(i);
+            }
+            intlist.ForEach(m => sum += m);
+            Console.WriteLine("sum:" + sum);
+            intlist.ForEach((m) => max = m > max ? m : max);
+            Console.WriteLine("max:" + max);
+            intlist.ForEach(m => min = m < min ? m : min);
+            Console.WriteLine("min:" + min);
+            Console.ReadKey();
+        }
+    }
+}
+
---
+++ Homework4.2/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ClockEvent
+{
+    class ClockEvent
+    {
+        public delegate void AlarmEvent();
+        public delegate void TickEvent();
+        public event AlarmEvent alarmEvent;
+        public event TickEvent tickEvent;
+        public int AHour, AMinute, ASecond = 0;
+        public void SetAlarm(int h, int m, int s)
+        {
+            AHour = h;
+            AMinute = m;
+            ASecond = s;
+        }
+        public void Tick()
+        {
+            tickEvent();
+        }
+        public void Alarm()
+        {
+            alarmEvent();
+        }
+    }
+    class ClockStart
+    {
+        public ClockEvent myClock = new ClockEvent();
+
+        public void StartWork()
+        {
+
+            while (true)
+            {
+                myClock.Tick();
+                int hour = DateTime.Now.Hour;
+                int minute = DateTime.Now.Minute;
+                int second = DateTime.Now.Second;
+                if (myClock.AHour == hour && myClock.AMinute == minute && myClock.ASecond == second)
+                {
+                    myClock.Alarm();
+                }
+                Thread.Sleep(1000);
+            }
+
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ClockStart clockstart = new ClockStart();
+            clockstart.myClock.alarmEvent += Alarming;
+            clockstart.myClock.tickEvent += Ticking;
+            clockstart.myClock.SetAlarm(3, 7, 37);
+            clockstart.StartWork();
+        }
+        public static void Alarming()
+        {
+            Console.WriteLine("Alarm!!!");
+        }
+        public static void Ticking()
+        {
+            Console.WriteLine("Tick");
+        }
+    }
+}
+
